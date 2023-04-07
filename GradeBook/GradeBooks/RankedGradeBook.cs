aa5e4a2a0fd9b4bdbf8c66ba63d59af0943fbd29
Jurek <jbujak@microsoft.wsei.edu.pl>
2023-04-07 15:33:42 +0200

    update

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,6 +10,35 @@ namespace GradeBook.GradeBooks
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name) { Type = GradeBookType.Ranked; }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if (averageGrade <= 20)
+            {
+                return 'F';
+            }else if(averageGrade > 20 && averageGrade<=40)
+            {
+                return 'D';
+            }  
+            else if(averageGrade > 40 && averageGrade<=60)
+            {
+                return 'C';
+            }
+            else if (averageGrade > 60 && averageGrade <= 80)
+            {
+                return 'B';
+            }
+            else
+            {
+                return 'A';
+            }
+
+        }
     }
 
 
